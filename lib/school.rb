d94fc618831c150d8d03d821d51d0521de9073aa
Harleigh Abel <harleighabel@gmail.com>
2018-02-14 21:48:38 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,14 +1,12 @@
 class School
-  ROSTER = {}
+  
   
   def initialize(name)
     @name= name
-    roster 
+    ROSTER = {}
   end
   
-  def roster
-    @roster = ROSTER
-  end  
+ 
   
   def add_student(student, grade)
     @roster[student] = []
