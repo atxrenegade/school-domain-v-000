7c2a0eda7368ed7e06f79c76ee99f71628ac8227
Harleigh Abel <harleighabel@gmail.com>
2018-02-14 21:51:38 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -11,8 +11,8 @@ class School
   end  
   
   def add_student(student, grade)
-    @roster[student] = []
-    @roster[student] << grade
+    @roster[grade] = []
+    @roster[grade] << student
   end  
     
 end  
